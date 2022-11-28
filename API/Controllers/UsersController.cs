e5ea5dcd36c9ff0e5dd5ce29ce34f4ea82b62551
nguyenhathien <Ducanh68cs@gmail.com>
2022-11-28 16:40:18 +0700

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -24,6 +24,8 @@ namespace API.Controllers
             return await _context.Users.ToListAsync();
         }
         [HttpGet("{id}")]
+
+
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
--- API/Startup.cs
+++ API/Startup.cs
@@ -33,26 +33,26 @@ namespace API
             });
             
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddCors();
+          
             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            // }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
